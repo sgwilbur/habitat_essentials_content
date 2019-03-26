0b77402fce1d0bd3ca71ba4c08bbdcc987fa3204
Sean G Wilbur <sgwilbur@gmail.com>
2019-03-26 16:07:37 -0500

    Updating svc user.

--- nodejs_demo/habitat/plan.sh
+++ nodejs_demo/habitat/plan.sh
@@ -1,9 +1,9 @@
 pkg_name=nodejs_demo
 pkg_origin=sgwilbur-20190325
-pkg_version="0.3.10"
+pkg_version="0.3.11"
 pkg_scaffolding="core/scaffolding-node"
 
-pkg_svc_user=hab
+pkg_svc_user=root
 pkg_svc_group=$pkg_svc_user
 
 pkg_exports=(
